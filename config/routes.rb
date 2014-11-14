eb9ddef471aa5d837b3a4ef00ce2d34886df4357
Shawn Plew <shawn.plew@mobiwm.com>
2014-11-13 20:09:22 -0500

    set up notes create and update actions

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,13 @@ Rails.application.routes.draw do
   resources :users, only: :create
   get 'sign_up' => 'users#new', as: :sign_up
   resources :sessions, only: :create
+  resources :notes
   delete 'logout' => 'sessions#destroy', as: :logout
   get 'login' => 'sessions#new', as: :login
+
+  # This is totally a thing we could do with notes.
+  # resources :users do
+  #   # /users/1/notes
+  #   resources :notes
+  # end
 end
